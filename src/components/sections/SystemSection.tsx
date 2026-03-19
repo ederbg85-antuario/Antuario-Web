38ea1f7db27102e415680c497fa5df9568d26783
ederbg85-antuario <ederbg85@gmail.com>
2026-03-19 15:56:20 -0600

    add SystemSection.tsx

---
+++ src/components/sections/SystemSection.tsx
@@ -0,0 +1,10 @@
+import SectionWrapper from '@/components/common/SectionWrapper'
+import { Megaphone, Settings, TrendingUp, ArrowRight, CheckCircle2 } from 'lucide-react'
+
+export default function SystemSection() {
+  return (
+    <SectionWrapper id="sistema">
+      <p>System Section</p>
+    </SectionWrapper>
+  )
+}
